e3c7e0d31ffe8ddba175d870e1c77e16c6b7a2c5
Joshua Saliutama <jsaliutama@gmail.com>
2017-01-17 10:49:12 +0700

    Update FormOutput.cs
    This is the initial update for FormOutput. Just add documentation.

--- FormOutput.cs
+++ FormOutput.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,11 +17,25 @@ namespace MajuMandiriGudang
             InitializeComponent();
         }
 
+        /**
+         * buttonOutDelivery_Click
+         * This function will handle the event and the transition between the output main menu and the output
+         * delivery item when the "Out Delivery" button is clicked.
+         * 
+         * Date last updated: 17 January 2017
+         */ 
         private void buttonOutDelivery_Click(object sender, EventArgs e)
         {
 
         }
 
+        /**
+        * buttonOutDelivery_Click
+        * This function will handle the event and the transition between the output main menu and the output
+        * delivery item when the "Out Return" button is clicked.
+        * 
+        * Date last updated: 17 January 2017
+        */
         private void buttonOutReturn_Click(object sender, EventArgs e)
         {
 
